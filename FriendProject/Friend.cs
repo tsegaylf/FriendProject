566157144eb8a37b6decba8d2d3925e31ba62d11
Lidya Tsegay <lidyatsegay@outlook.com>
2019-08-29 14:45:56 -0400

    initial commit

---
+++ FriendProject/Friend.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FriendProject {
+    class Friend {
+
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool BFF { get; set; } = false;
+        public int HowLong { get; set; } 
+
+
+        public Friend(string name, string phone, string email, bool bff, int howlong) { // create default constructor
+            this.Name = name;
+            this.Phone = phone;
+            this.Email = email;
+            this.BFF = bff;
+            this.HowLong = howlong;
+        }
+
+        public Friend() { // default constructor
+                }
+
+        //int 1 =0;
+        //i.ToString();
+        //bool b = true;
+        //bool.ToString
+
+        public override string ToString() {
+            return $"{Name, -15} {Phone, -12} {Email, -25} {(BFF ? "BFF" : ""), 3} {HowLong}"; //Interpolation string
+        }
+
+            // Is friend BFF or not 
+            public bool CheckBffStatus(bool isBff) {
+            if (isBff == true) {
+                Console.WriteLine("This person is my Best Friend");
+                return true;
+            }
+            return false;
+
+        }
+        //How long have you known friend
+       
+
+
+    }
+}
--- FriendProject/Program.cs
+++ FriendProject/Program.cs
@@ -3,7 +3,24 @@
 namespace FriendProject {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+
+            var F1 = new Friend("Chiamaka", "513-456-3456", "chi@gmail.com", true, 3);
+            var F2 = new Friend("Yohanna", "413-456-3456", "yoh@gmail.com", true, 2);
+            var F3 = new Friend("Abel", "613-456-3456", "abel@gmail.com", true, 5);
+            var F4 = new Friend("Erica", "613-456-3456", "erica@gmail.com", false, 1);
+            var F5 = new Friend("Ashley", "713-456-3456", "ash@gmail.com", false, 4);
+
+            var friends = new Friend[] { F1, F2, F3, F4, F5};
+
+             foreach (var friend in friends) {
+                Console.WriteLine(friend);
+                }
+        
+
+
+
+
+
         }
     }
 }
